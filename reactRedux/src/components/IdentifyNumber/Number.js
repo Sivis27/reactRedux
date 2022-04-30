4632dd807b9e41d2cca4f7aec965cf96b552a720
vimaladevi <atom.vimaladevi@gmail.com>
2022-04-30 06:38:56 +0530

    Pages Added

---
+++ reactRedux/src/components/IdentifyNumber/Number.js
@@ -0,0 +1,21 @@
+let fruitsObject = [
+  "Apple",
+  "Strawberry",
+  "Mango",
+  "Grapes",
+  "Lemon",
+  "PineApple",
+  "Pomegranate",
+  "Pappaya",
+  "Orange",
+  "Banana",
+];
+
+console.log('FruitObj - ',fruitsObject);
+
+//1.reason of group combination it can be 4 / more / less than 4 
+// ABCD
+//ABC
+//AB
+//A
+
